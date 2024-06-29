243aa69f3a10fcfc739d46434b919ec9b422e746
Tigran Mesropyan <tikodeveloper@gmail.com>
2024-06-29 22:41:30 +0400

    added godoc

--- adder.go
+++ adder.go
@@ -1,5 +1,12 @@
+// Package adder provides basic mathematical operations.
+// This package includes a simple function to add two integers.
+
 package adder
 
+// Add takes two integers and returns their sum.
+// This function performs a basic addition operation.
+// For more information about addition, you can visit:
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
